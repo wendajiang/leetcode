30cc886ea7a187c89a3bd7ded27622aff6e4e693
wendajiang <wendajiang93@163.com>
2017-07-03 13:12:41 +0800

    1.Two Sum

---
+++ 1.Two Sum/1.two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+
+        map<int, int> mapping;
+
+        for(int i = 0; i < nums.size(); i++){
+            mapping[nums[i]] = i;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            int searched = target - nums[i];
+            if(mapping.find(searched) != mapping.end() && i != mapping[searched]){
+                result.push_back(i);
+                result.push_back(mapping[searched]);
+                break;
+            }
+        }
+        return result;
+    }
+};
