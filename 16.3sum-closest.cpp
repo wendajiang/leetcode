ccf6e4cb8ec55c3bfe7fb39e4a2fe0ea9420efdc
wendajiang <wendajiang93@163.com>
2017-07-08 20:31:02 +0800

    autocomplete clang

--- 16.3sum-closest.cpp
+++ 16.3sum-closest.cpp
@@ -4,19 +4,19 @@
  * https://leetcode.com/problems/3sum-closest
  *
  * Medium (30.96%)
- * Total Accepted:    
- * Total Submissions: 
+ * Total Accepted:
+ * Total Submissions:
  * Testcase Example:  '[0,0,0]\n1'
  *
  * Given an array S of n integers, find three integers in S such that the sum
  * is closest to a given number, target. Return the sum of the three integers.
  * You may assume that each input would have exactly one solution.
- * 
- * 
+ *
+ *
  * ⁠   For example, given array S = {-1 2 1 -4}, and target = 1.
- * 
+ *
  * ⁠   The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
- * 
+ *
  */
 class Solution {
 public:
@@ -49,7 +49,7 @@ public:
                 }
             }
             if(record == target) break;
-            //de-duplicate 
+            //de-duplicate
             while(i < len - 1 && nums[i] == nums[i + 1]) i++;
         }
         return record;
