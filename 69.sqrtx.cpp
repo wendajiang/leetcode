20af320c39c0f6a275267dbae2e7932ff2fa4f7e
wendajiang <wendajiang93@163.com>
2017-07-08 17:29:23 +0800

    nothing

---
+++ 69.sqrtx.cpp
@@ -0,0 +1,20 @@
+/*
+ * [69] Sqrt(x)
+ *
+ * https://leetcode.com/problems/sqrtx
+ *
+ * Easy (27.67%)
+ * Total Accepted:    
+ * Total Submissions: 
+ * Testcase Example:  '0'
+ *
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x.
+ */
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+    }
+};
