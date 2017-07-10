24c120fd3d6f44ee88571ef2045e8b3b52983124
wendajiang <wendajiang93@163.com>
2017-07-10 12:44:14 +0800

    位运算

---
+++ 191.number-of-1-bits.cpp
@@ -0,0 +1,30 @@
+/*
+ * [191] Number of 1 Bits
+ *
+ * https://leetcode.com/problems/number-of-1-bits
+ *
+ * Easy (39.38%)
+ * Total Accepted:
+ * Total Submissions:
+ * Testcase Example:  '           0 (00000000000000000000000000000000)'
+ *
+ * Write a function that takes an unsigned integer and returns the number of
+ * ’1' bits it has (also known as the Hamming weight).
+ *
+ * For example, the 32-bit integer ’11' has binary representation
+ * 00000000000000000000000000001011, so the function should return 3.
+ *
+ * Credits:Special thanks to @ts for adding this problem and creating all test
+ * cases.
+ */
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+        while(n){
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+    }
+};
