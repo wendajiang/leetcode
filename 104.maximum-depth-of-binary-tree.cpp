28401b437c83630b93c6cb339160b5f09118edd1
wendajiang <wendajiang93@163.com>
2017-07-11 13:06:44 +0800

    加入BFS的解法

--- 104.maximum-depth-of-binary-tree.cpp
+++ 104.maximum-depth-of-binary-tree.cpp
@@ -26,6 +26,35 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
-        else return std::max((1 + maxDepth(root->left)),(1 + maxDepth(root->right)));
+        else if(!root->left && !root->right) return std::max((1 + maxDepth(root->left)),(1 + maxDepth(root->right)));
     }
+
+/*
+int maxDepth(TreeNode *root)
+{
+    if(root == NULL)
+        return 0;
+
+    int res = 0;
+    queue<TreeNode *> q;
+    q.push(root);
+    while(!q.empty())
+    {
+        ++ res;
+        for(int i = 0, n = q.size(); i < n; ++ i)
+        {
+            TreeNode *p = q.front();
+            q.pop();
+
+            if(p -> left != NULL)
+                q.push(p -> left);
+            if(p -> right != NULL)
+                q.push(p -> right);
+        }
+    }
+
+    return res;
+}
+
+*/
 };
