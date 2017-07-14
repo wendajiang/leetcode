c46e2d2d0933a25c868be1deb27265046dfea470
wendajiang <wendajiang93@163.com>
2017-07-14 16:18:05 +0800

    定制操作

---
+++ 56.merge-intervals.cpp
@@ -0,0 +1,44 @@
+/*
+ * [56] Merge Intervals
+ *
+ * https://leetcode.com/problems/merge-intervals
+ *
+ * Medium (29.71%)
+ * Total Accepted:
+ * Total Submissions:
+ * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+ *
+ * Given a collection of intervals, merge all overlapping intervals.
+ *
+ *
+ * For example,
+ * Given [1,3],[2,6],[8,10],[15,18],
+ * return [1,6],[8,10],[15,18].
+ *
+ */
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        if (intervals.empty()) return vector<Interval>{};
+        vector<Interval> res;
+        //去看通用算法的比较函数怎么写
+        
+        sort(intervals.begin(), intervals.end(), [](Interval a, Interval b){return a.start < b.start;});
+        res.push_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (res.back().end < intervals[i].start) res.push_back(intervals[i]);
+            else
+                res.back().end = max(res.back().end, intervals[i].end);
+            }
+        return res;
+    }
+};
