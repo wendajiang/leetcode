95eee6f8f21b0b89d8a4985fd39a5f83c3af916e
wendajiang <wendajiang93@163.com>
2017-07-17 15:17:44 +0800

    nothing

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
-
-
         map<int, int> mapping;
 
         for(int i = 0; i < nums.size(); i++){
