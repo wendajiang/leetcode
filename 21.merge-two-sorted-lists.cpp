57efaabc1fdfbe0940da75f68eac4e62586e5fff
wendajiang <wendajiang93@163.com>
2017-07-18 15:35:25 +0800

    合并两个排序链表，然后扩展为合并多个排序链表（递归解法）

--- 21.merge-two-sorted-lists.cpp
+++ 21.merge-two-sorted-lists.cpp
@@ -21,41 +21,55 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL)
-            return l2;
-        if(l2 == NULL)
+
+
+    ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (NULL == l1) return l2;
+        else if (NULL == l2) return l1;
+        if (l1->val <= l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
             return l1;
-        ListNode* result = new ListNode((l1->val < l2->val) ? l1->val : l2->val);
-        if(l1->val < l2->val)
-            l1 = l1->next;
-        else
-            l2 = l2->next;
-        ListNode* head = result;
-        while(l1 != NULL && l2 != NULL){
-            if(l1->val < l2->val){
-                result->next = new ListNode(l1->val);
-                result = result->next;
-                l1 = l1->next;
-            }else {
-                result->next = new ListNode(l2->val);
-                result = result->next;
-                l2 = l2->next;
-            }
         }
-        if(l1 == NULL){
-            while(l2 != NULL){
-                result->next = new ListNode(l2->val);
-                result = result->next;
-                l2 = l2->next;
-            }
-        }else if(l2 == NULL){
-            while(l1 != NULL){
-                result->next = new ListNode(l1->val);
-                result = result->next;
-                l1 = l1->next;
-            }
+        else {
+            l2->next = mergeTwoLists(l1, l2->next);
+            return l2;
         }
-        return head;
     }
+    // ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    //     if(l1 == NULL)
+    //         return l2;
+    //     if(l2 == NULL)
+    //         return l1;
+    //     ListNode* result = new ListNode((l1->val < l2->val) ? l1->val : l2->val);
+    //     if(l1->val < l2->val)
+    //         l1 = l1->next;
+    //     else
+    //         l2 = l2->next;
+    //     ListNode* head = result;
+    //     while(l1 != NULL && l2 != NULL){
+    //         if(l1->val < l2->val){
+    //             result->next = new ListNode(l1->val);
+    //             result = result->next;
+    //             l1 = l1->next;
+    //         }else {
+    //             result->next = new ListNode(l2->val);
+    //             result = result->next;
+    //             l2 = l2->next;
+    //         }
+    //     }
+    //     if(l1 == NULL){
+    //         while(l2 != NULL){
+    //             result->next = new ListNode(l2->val);
+    //             result = result->next;
+    //             l2 = l2->next;
+    //         }
+    //     }else if(l2 == NULL){
+    //         while(l1 != NULL){
+    //             result->next = new ListNode(l1->val);
+    //             result = result->next;
+    //             l1 = l1->next;
+    //         }
+    //     }
+    //     return head;
+    // }
 };
