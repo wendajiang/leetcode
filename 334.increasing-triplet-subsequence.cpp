13167470314af41e07455107b268a087cda10512
wendajiang <wendajiang93@163.com>
2017-07-19 15:40:46 +0800

    增长递增子序列的特殊情况

---
+++ 334.increasing-triplet-subsequence.cpp
@@ -0,0 +1,57 @@
+/*
+ * [334] Increasing Triplet Subsequence
+ *
+ * https://leetcode.com/problems/increasing-triplet-subsequence
+ *
+ * Medium (39.00%)
+ * Total Accepted:
+ * Total Submissions:
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ *
+ * Given an unsorted array return whether an increasing subsequence of length 3
+ * exists or not in the array.
+ *
+ *
+ * Formally the function should:
+ * Return true if there exists i, j, k
+ * such that arr[i] < arr[j] < arr[k] given 0 ≤ i < j < k ≤ n-1
+ * else return false.
+ *
+ *
+ *
+ * Your algorithm should run in O(n) time complexity and O(1) space
+ * complexity.
+ *
+ *
+ * Examples:
+ * Given [1, 2, 3, 4, 5],
+ * return true.
+ *
+ *
+ * Given [5, 4, 3, 2, 1],
+ * return false.
+ *
+ *
+ * Credits:Special thanks to @DjangoUnchained for adding this problem and
+ * creating all test cases.
+ */
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if(nums.size() < 3)
+            return false;
+        int minV = nums[0];
+        int second = INT_MAX;
+        for(int i = 0; i < nums.size(); i ++){
+            minV = std::min(minV, nums[i]);
+            if(nums[i] > minV){
+                second = std::min(second, nums[i]);
+            }
+            if(nums[i] > second){
+                return true;
+            }
+        }
+        return false;
+    }
+};
