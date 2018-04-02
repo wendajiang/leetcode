a6da350d6e62a8cdde915c625adce990cbcd8d5e
wendajiang <wendajiang93@163.com>
2018-04-02 22:21:53 +0800

    again write some review the kth small in matrix

--- 378.kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378.kth-smallest-element-in-a-sorted-matrix.cpp
@@ -34,38 +34,57 @@
  */
 class Solution {
 public:
-    // klog(n) priority queue solution
-    struct compare
-    {
-        bool operator()(const pair<int,pair<int, int> >& a, const pair<int,pair<int, int> >& b)
-        {
-            return a.first>b.first;
+    // // klog(n) priority queue solution
+    // struct compare
+    // {
+    //     bool operator()(const pair<int,pair<int, int> >& a, const pair<int,pair<int, int> >& b)
+    //     {
+    //         return a.first>b.first;
+    //     }
+    // };
+    // int kthSmallest(vector<vector<int>>& arr, int k) {
+    //
+    //     int n=arr.size(),m=arr[0].size();
+    //
+    //     priority_queue< pair<int,pair<int, int> >, vector<pair<int, pair<int, int> > >, compare > p;
+    //
+    //     for(int i=0;i<n;i++)
+    //     p.push(make_pair(arr[i][0],make_pair(i,0)));
+    //
+    //     int x=k,ans;
+    //     while(x--)
+    //     {
+    //         int e=p.top().first;
+    //         int i=p.top().second.first;
+    //         int j=p.top().second.second;
+    //         ans=e;
+    //         p.pop();
+    //         if(j!=m-1)
+    //         p.push(make_pair(arr[i][j+1],make_pair(i,j+1)));
+    //     }
+    //     return ans;
+    //
+    // }
+
+    int kthSmallest(vector<vector<int>>& matrix, int k){
+        int n = matrix.size();
+        int l = matrix[0][0], r = matrix[n - 1][n - 1];
+        int mid = 0;
+        while(l < r){
+            mid = l + (r - l) / 2;
+            int num = 0; // number of elements no bigger than mid
+            for(int i = 0; i < n; i++){
+                int pos = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+                num += pos;
+            }
+            if(num < k){
+                l = mid + 1;
+            }else{
+                r = mid;
+            }
         }
-    };
-    int kthSmallest(vector<vector<int>>& arr, int k) {
-
-        int n=arr.size(),m=arr[0].size();
-
-        priority_queue< pair<int,pair<int, int> >, vector<pair<int, pair<int, int> > >, compare > p;
-
-        for(int i=0;i<n;i++)
-        p.push(make_pair(arr[i][0],make_pair(i,0)));
-
-        int x=k,ans;
-        while(x--)
-        {
-            int e=p.top().first;
-            int i=p.top().second.first;
-            int j=p.top().second.second;
-            ans=e;
-            p.pop();
-            if(j!=m-1)
-            p.push(make_pair(arr[i][j+1],make_pair(i,j+1)));
-        }
-        return ans;
-
+        return l;
     }
-
 
     // //BS solution
     // int kthSmallest(vector<vector<int>>& matrix, int k)
