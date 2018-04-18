8b515e364cb03b384b3c5bdee01dec02c69b0eb2
wendajiang <wendajiang93@163.com>
2018-04-18 08:56:24 +0800

    nothing

---
+++ 804.unique-morse-code-words.cpp
@@ -0,0 +1,69 @@
+/*
+ * [822] Unique Morse Code Words
+ *
+ * https://leetcode.com/problems/unique-morse-code-words/description/
+ *
+ * algorithms
+ * Easy (76.44%)
+ * Total Accepted:    9.2K
+ * Total Submissions: 12.1K
+ * Testcase Example:  '["gin", "zen", "gig", "msg"]'
+ *
+ * International Morse Code defines a standard encoding where each letter is
+ * mapped to a series of dots and dashes, as follows: "a" maps to ".-", "b"
+ * maps to "-...", "c" maps to "-.-.", and so on.
+ *
+ * For convenience, the full table for the 26 letters of the English alphabet
+ * is given below:
+ *
+ *
+ *
+ * [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+ *
+ * Now, given a list of words, each word can be written as a concatenation of
+ * the Morse code of each letter. For example, "cba" can be written as
+ * "-.-.-....-", (which is the concatenation "-.-." + "-..." + ".-"). We'll
+ * call such a concatenation, the transformation of a word.
+ *
+ * Return the number of different transformations among all words we have.
+ *
+ *
+ * Example:
+ * Input: words = ["gin", "zen", "gig", "msg"]
+ * Output: 2
+ * Explanation:
+ * The transformation of each word is:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ *
+ * There are 2 different transformations, "--...-." and "--...--.".
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * The length of words will be at most 100.
+ * Each words[i] will have length in range [1, 12].
+ * ⁠   words[i] will only consist of lowercase letters.
+ *
+ *
+ */
+class Solution {
+public:
+    vector<string> flag{".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    int uniqueMorseRepresentations(vector<string>& words) {
+        set<string> res;
+        for(auto &s : words){
+            string morse = "";
+            for(auto &c : s){
+                morse += flag[c - 'a'];
+            }
+            res.insert(morse);
+        }
+        return res.size();
+    }
+};
