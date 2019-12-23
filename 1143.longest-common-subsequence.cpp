9ed964cd91291972cda9d88dc369c406b7c52d07
wendajiang <wendajiang93@163.com>
2019-12-23 23:23:57 +0800

    LCS

---
+++ 1143.longest-common-subsequence.cpp
@@ -0,0 +1,94 @@
+/*
+ * @lc app=leetcode id=1143 lang=cpp
+ *
+ * [1143] Longest Common Subsequence
+ *
+ * https://leetcode.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (57.64%)
+ * Likes:    415
+ * Dislikes: 10
+ * Total Accepted:    25.8K
+ * Total Submissions: 44.8K
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * Given two strings text1 and text2, return the length of their longest common
+ * subsequence.
+ * 
+ * A subsequence of a string is a new string generated from the original string
+ * with some characters(can be none) deleted without changing the relative
+ * order of the remaining characters. (eg, "ace" is a subsequence of "abcde"
+ * while "aec" is not). A common subsequence of two strings is a subsequence
+ * that is common to both strings.
+ * 
+ * 
+ * 
+ * If there is no common subsequence, return 0.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: text1 = "abcde", text2 = "ace" 
+ * Output: 3  
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: text1 = "abc", text2 = "abc"
+ * Output: 3
+ * Explanation: The longest common subsequence is "abc" and its length is 3.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: text1 = "abc", text2 = "def"
+ * Output: 0
+ * Explanation: There is no such common subsequence, so the result is 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= text1.length <= 1000
+ * 1 <= text2.length <= 1000
+ * The input strings consist of lowercase English characters only.
+ * 
+ * 
+ */
+
+#include <bits/stdc++.h>
+// @lc code=start
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int len1 = text1.size();
+        int len2 = text2.size();
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int i = 0; i <= len2; i++) {
+            dp[0][i] = 0;
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if(text1.at(i - 1) == text2.at(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } 
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+    // const int max_n = 1000 + 5;
+#define max_n 1005
+    int dp[max_n][max_n];
+};
+// @lc code=end
