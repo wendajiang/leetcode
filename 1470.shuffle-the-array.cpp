68a4343e7dac43a69cac1c07a940ea283ac5051d
wendajiang <wendajiang93@163.com>
2020-11-08 14:44:51 +0800

    nothing, only write

---
+++ 1470.shuffle-the-array.cpp
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode id=1470 lang=cpp
+ *
+ * [1470] Shuffle the Array
+ *
+ * https://leetcode.com/problems/shuffle-the-array/description/
+ *
+ * algorithms
+ * Easy (88.55%)
+ * Likes:    681
+ * Dislikes: 75
+ * Total Accepted:    114.8K
+ * Total Submissions: 129.7K
+ * Testcase Example:  '[2,5,1,3,4,7]\n3'
+ *
+ * Given the array nums consisting of 2n elements in the form
+ * [x1,x2,...,xn,y1,y2,...,yn].
+ * 
+ * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,5,1,3,4,7], n = 3
+ * Output: [2,3,5,4,1,7] 
+ * Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is
+ * [2,3,5,4,1,7].
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [1,2,3,4,4,3,2,1], n = 4
+ * Output: [1,4,2,3,3,2,4,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [1,1,2,2], n = 2
+ * Output: [1,2,1,2]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 500
+ * nums.length == 2n
+ * 1 <= nums[i] <= 10^3
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> res;
+        for (int i = 0; i < n; i++) {
+            res.push_back(nums[i]);
+            res.push_back(nums[i + n]);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
