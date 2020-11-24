f27b0933536f58619f6c82f367469032cc51729d
wendajiang <wendajiang93@163.com>
2020-11-25 01:25:40 +0800

    @todo opt; DFS求强联通分量个数

---
+++ 1319.number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,117 @@
+/*
+ * @lc app=leetcode id=1319 lang=cpp
+ *
+ * [1319] Number of Operations to Make Network Connected
+ *
+ * https://leetcode.com/problems/number-of-operations-to-make-network-connected/description/
+ *
+ * algorithms
+ * Medium (54.63%)
+ * Likes:    613
+ * Dislikes: 12
+ * Total Accepted:    24.4K
+ * Total Submissions: 44.7K
+ * Testcase Example:  '4\n[[0,1],[0,2],[1,2]]'
+ *
+ * There are n computers numbered from 0 to n-1 connected by ethernet cables
+ * connections forming a network where connections[i] = [a, b] represents a
+ * connection between computers a and b. Any computer can reach any other
+ * computer directly or indirectly through the network.
+ * 
+ * Given an initial computer network connections. You can extract certain
+ * cables between two directly connected computers, and place them between any
+ * pair of disconnected computers to make them directly connected. Return the
+ * minimum number of times you need to do this in order to make all the
+ * computers connected. If it's not possible, return -1. 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+ * Output: 1
+ * Explanation: Remove cable between computer 1 and 2 and place between
+ * computers 1 and 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+ * Output: 2
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+ * Output: -1
+ * Explanation: There are not enough cables.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: n = 5, connections = [[0,1],[0,2],[3,4],[2,3]]
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 10^5
+ * 1 <= connections.length <= min(n*(n-1)/2, 10^5)
+ * connections[i].length == 2
+ * 0 <= connections[i][0], connections[i][1] < n
+ * connections[i][0] != connections[i][1]
+ * There are no repeated connections.
+ * No two computers are connected by more than one cable.
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+   
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int edges = connections.size();
+        if (edges < n - 1) return -1;
+        vector<vector<int>> G(n, vector<int>());
+        // 无向边
+        for (int i = 0; i < connections.size(); i++) {
+            G[connections[i][0]].push_back(connections[i][1]);
+            G[connections[i][1]].push_back(connections[i][0]);
+        }
+        //@ 求强联通分量的个数输出
+        vector<bool> visit(n, false);
+        int cur_i = 0;
+        int res = 0;
+        dfs(0, visit, G);
+        for (; cur_i < n; cur_i++) {
+            if (visit[cur_i] == false) {
+                dfs(cur_i, visit, G);
+                res ++;
+            }
+        }
+        return res;
+    }
+
+    void dfs(int i, vector<bool>& v, vector<vector<int>>& G) {
+        v[i] = true;
+        for (int l = 0; l < G[i].size(); l++) {
+            if (v[G[i][l]] == false) {
+                dfs(G[i][l], v, G);
+            }
+        }
+    }
+};
+// @lc code=end
+
