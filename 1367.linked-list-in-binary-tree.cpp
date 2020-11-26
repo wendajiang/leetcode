ff722de09d8a122dd9bf49c7e63e049eb1e411f1
wendajiang <wendajiang93@163.com>
2020-11-26 23:51:18 +0800

    递归

---
+++ 1367.linked-list-in-binary-tree.cpp
@@ -0,0 +1,142 @@
+/*
+ * @lc app=leetcode id=1367 lang=cpp
+ *
+ * [1367] Linked List in Binary Tree
+ *
+ * https://leetcode.com/problems/linked-list-in-binary-tree/description/
+ *
+ * algorithms
+ * Medium (41.16%)
+ * Likes:    589
+ * Dislikes: 22
+ * Total Accepted:    22.8K
+ * Total Submissions: 55.4K
+ * Testcase Example:  '[4,2,8]\n[1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]'
+ *
+ * Given a binary tree root and a linked list with head as the first node. 
+ * 
+ * Return True if all the elements in the linked list starting from the head
+ * correspond to some downward path connected in the binary tree otherwise
+ * return False.
+ * 
+ * In this context downward path means a path that starts at some node and goes
+ * downwards.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * 
+ * 
+ * Input: head = [4,2,8], root =
+ * [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+ * Output: true
+ * Explanation: Nodes in blue form a subpath in the binary Tree.  
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * 
+ * 
+ * Input: head = [1,4,2,6], root =
+ * [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = [1,4,2,6,8], root =
+ * [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3]
+ * Output: false
+ * Explanation: There is no path in the binary tree that contains all the
+ * elements of the linked list from head.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= node.val <= 10\0 for each node in the linked list and binary tree.
+ * The given linked list will contain between 1 and 100 nodes.
+ * The given binary tree will contain between 1 and 2500 nodes.
+ * 
+ * 
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool ans = false;
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        ans = false;
+        if (head == nullptr) 
+            return true;
+        if (root == nullptr)
+            return false;
+        travel(root, head);
+        return ans;
+    }
+
+    void travel(TreeNode* root, ListNode* head) {
+        if (root->val == head->val && checkPath(head, root)) {
+            ans = true;
+            return;
+        }
+        if (root->left) travel(root->left, head);
+        if (root->right) travel(root->right, head);
+        
+    }
+
+    bool checkPath(ListNode* lnode, TreeNode* tnode) {
+        if (lnode == nullptr) 
+            return true;
+        if (tnode == nullptr) 
+            return false;
+        if (lnode->val != tnode->val)
+            return false;
+        return checkPath(lnode->next, tnode->left) || checkPath(lnode->next, tnode->right);
+    }
+};
+// @lc code=end
