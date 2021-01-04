85f5cc8347f35f592ddd9e8935239281c4942e17
johnwdjiang <johnwdjiang@tencent.com>
2021-01-04 10:13:03 +0800

    dfs?

--- 472.concatenated-words.cpp
+++ 472.concatenated-words.cpp
@@ -54,6 +54,39 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        vector<string> result;
+        if(words.empty()) return result; 
+        auto mycomp = [&](const string& str1, const string& str2){return str1.size() < str2.size();};
+        sort(words.begin(), words.end(), mycomp);
+        unordered_set<string> mp;
+        for(auto& word: words) {
+            string path = "";
+            if(dfs(word, 0, path, 0, mp)) result.push_back(word); // We don't need to insert this word, because it can be concatenated from other words.
+            else mp.insert(word); 
+        }
+        return result;
+    }
+    
+private:
+    bool dfs(string& word, int pos, string& path, int cnt, unordered_set<string>& mp) {
+        if(pos == word.size()) {
+            if(mp.find(path) != mp.end() && cnt > 0) return true;
+            else return false;
+        }
+        path.push_back(word[pos]);
+        if(mp.find(path) != mp.end()) {
+            string temp = "";
+            if(dfs(word, pos+1, temp, cnt + 1, mp)) return true;
+        }
+        if(dfs(word, pos+1, path, cnt, mp)) return true;
+        else return false;
+    }
+};
+
+/*
+class Solution {
+public:
     struct node {
         int ch[26];
         bool flag;
@@ -116,6 +149,7 @@ public:
         return res;
     }
 };
+*/
 
 // @lc code=end
 
