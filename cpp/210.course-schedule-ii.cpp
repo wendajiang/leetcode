fbcf958ac3ddf481da5e32234090b4aa84692fd6
David <wendajiang93@163.com>
2022-05-28 21:19:32 +0800

    review

--- cpp/210.course-schedule-ii.cpp
+++ cpp/210.course-schedule-ii.cpp
@@ -1,77 +1,89 @@
 /*
+ * @lc app=leetcode id=210 lang=cpp
+ *
  * [210] Course Schedule II
  *
  * https://leetcode.com/problems/course-schedule-ii/description/
  *
  * algorithms
- * Medium (30.37%)
- * Total Accepted:    91.8K
- * Total Submissions: 301.4K
+ * Medium (44.95%)
+ * Likes:    6730
+ * Dislikes: 237
+ * Total Accepted:    630.1K
+ * Total Submissions: 1.3M
  * Testcase Example:  '2\n[[1,0]]'
  *
- * There are a total of n courses you have to take, labeled from 0 to n-1.
+ * There are a total of numCourses courses you have to take, labeled from 0 to
+ * numCourses - 1. You are given an array prerequisites where prerequisites[i]
+ * = [ai, bi] indicates that you must take course bi first if you want to take
+ * course ai.
  *
- * Some courses may have prerequisites, for example to take course 0 you have
- * to first take course 1, which is expressed as a pair: [0,1]
  *
- * Given the total number of courses and a list of prerequisite pairs, return
- * the ordering of courses you should take to finish all courses.
+ * For example, the pair [0, 1], indicates that to take course 0 you have to
+ * first take course 1.
  *
- * There may be multiple correct orders, you just need to return one of them.
- * If it is impossible to finish all courses, return an empty array.
+ *
+ * Return the ordering of courses you should take to finish all courses. If
+ * there are many valid answers, return any of them. If it is impossible to
+ * finish all courses, return an empty array.
+ *
  *
  * Example 1:
  *
  *
- * Input: 2, [[1,0]]
+ * Input: numCourses = 2, prerequisites = [[1,0]]
  * Output: [0,1]
- * Explanation: There are a total of 2 courses to take. To take course 1 you
- * should have finished
- * course 0. So the correct course order is [0,1] .
+ * Explanation: There are a total of 2 courses to take. To take course 1 you
+ * should have finished course 0. So the correct course order is [0,1].
+ *
  *
  * Example 2:
  *
  *
- * Input: 4, [[1,0],[2,0],[3,1],[3,2]]
- * Output: [0,1,2,3] or [0,2,1,3]
- * Explanation: There are a total of 4 courses to take. To take course 3 you
- * should have finished both
- * ⁠            courses 1 and 2. Both courses 1 and 2 should be taken after you
- * finished course 0.
+ * Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+ * Output: [0,2,1,3]
+ * Explanation: There are a total of 4 courses to take. To take course 3 you
+ * should have finished both courses 1 and 2. Both courses 1 and 2 should be
+ * taken after you finished course 0.
  * So one correct course order is [0,1,2,3]. Another correct ordering is
- * [0,2,1,3] .
- *
- * Note:
+ * [0,2,1,3].
  *
  *
- * The input prerequisites is a graph represented by a list of edges, not
- * adjacency matrices. Read more about how a graph is represented.
- * You may assume that there are no duplicate edges in the input
- * prerequisites.
+ * Example 3:
  *
  *
- * Hints:
+ * Input: numCourses = 1, prerequisites = []
+ * Output: [0]
  *
  *
- * This problem is equivalent to finding the topological order in a directed
- * graph. If a cycle exists, no topological ordering exists and therefore it
- * will be impossible to take all courses.
- * Topological Sort via DFS - A great video tutorial (21 minutes) on Coursera
- * explaining the basic concepts of Topological Sort.
- * Topological sort could also be done via BFS.
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= numCourses <= 2000
+ * 0 <= prerequisites.length <= numCourses * (numCourses - 1)
+ * prerequisites[i].length == 2
+ * 0 <= ai, bi < numCourses
+ * ai != bi
+ * All the pairs [ai, bi] are distinct.
  *
  *
  */
+
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
 class Solution {
-public:
-    void dfs(int u, vector<vector<int>>& G, vector<int>& vis, bool& isCircle, vector<int>& res){
-        if(isCircle) return;
+   public:
+    void dfs(int u, vector<vector<int>>& G, vector<int>& vis, bool& isCircle,
+             vector<int>& res) {
+        if (isCircle) return;
         vis[u] = -1;
-        for(int i = 0; i < G[u].size(); i++){
-            if(vis[G[u][i]] != 1){
-                if(vis[G[u][i]] == 0){
+        for (int i = 0; i < G[u].size(); i++) {
+            if (vis[G[u][i]] != 1) {
+                if (vis[G[u][i]] == 0) {
                     dfs(G[u][i], G, vis, isCircle, res);
-                }else{
+                } else {
                     isCircle = true;
                     return;
                 }
@@ -81,24 +93,65 @@ public:
         res.push_back(u);
     }
 
-    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
-        vector<vector<int>> G(numCourses, vector<int>());
-        for(int i = 0; i < prerequisites.size(); i++){
-            int from = prerequisites[i].first;
-            int to = prerequisites[i].second;
-            G[from].push_back(to);
+    // 出度 dfs
+    // vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites)
+    // {
+    //     vector<vector<int>> G(numCourses, vector<int>());
+    //     for (int i = 0; i < prerequisites.size(); i++) {
+    //         int from = prerequisites[i][0];
+    //         int to = prerequisites[i][1];
+    //         G[from].push_back(to);
+    //     }
+
+    //     vector<int> res;
+    //     bool isCircle = false;
+    //     vector<int> vis(numCourses, 0);
+    //     for (int i = 0; i < numCourses; i++) {
+    //         if (!vis[i]) {
+    //             dfs(i, G, vis, isCircle, res);
+    //         }
+    //     }
+
+    //     if (!isCircle)
+    //         return res;
+    //     else
+    //         return vector<int>();
+    // }
+
+    // kahn's algorithm
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> edges(numCourses, vector<int>{});
+        vector<int> indegrees(numCourses, 0);
+        deque<int> queue;
+        for (auto p : prerequisites) {
+            edges[p[1]].push_back(p[0]);
+            indegrees[p[0]] += 1;
         }
 
+        for (int i = 0; i < numCourses; i++) {
+            if (indegrees[i] == 0) {
+                queue.push_back(i);
+            }
+        }
         vector<int> res;
-        bool isCircle = false;
-        vector<int> vis(numCourses, 0);
-        for(int i = 0;i < numCourses; i++){
-            if(!vis[i]){
-                dfs(i, G, vis, isCircle, res);
+        while (!queue.empty()) {
+            int u = queue.front();
+            queue.pop_front();
+            res.push_back(u);
+
+            for (auto v : edges[u]) {
+                indegrees[v] -= 1;
+                if (indegrees[v] == 0) {
+                    queue.push_back(v);
+                }
             }
         }
 
-        if(!isCircle) return res;
-        else return  vector<int>();
+        if (res.size() == numCourses) {
+            return res;
+        } else {
+            return vector<int>{};
+        }
     }
 };
+// @lc code=end
--- cpp/52.n-queens-ii.cpp
+++ cpp/52.n-queens-ii.cpp
@@ -44,22 +44,24 @@
  *
  *
  */
+#include <bits/stdc++.h>
+using namespace std;
 
 // @lc code=start
 class Solution {
-public:
+   public:
     int totalNQueens(int n) {
         cols = vector<bool>(n, 0);
         diag1 = vector<bool>(2 * n - 1, 0);
         diag2 = vector<bool>(2 * n - 1, 0);
 
         ans = 0;
-        nqueens(0, n);
+        nQueens(0, n);
 
         return ans;
     }
 
-private:
+   private:
     vector<bool> cols;
     vector<bool> diag1;
     vector<bool> diag2;
@@ -75,8 +77,7 @@ private:
         diag2[x - y + n - 1] = is_put;
     }
 
-
-    void nqueens(int y, int n) {
+    void nQueens(int y, int n) {
         if (y == n) {
             ans += 1;
             return;
@@ -85,7 +86,7 @@ private:
         for (int x = 0; x < n; x++) {
             if (!available(x, y, n)) continue;
             updateBoard(x, y, n, true);
-            nqueens(y + 1, n);
+            nQueens(y + 1, n);
             updateBoard(x, y, n, false);
         }
     }
