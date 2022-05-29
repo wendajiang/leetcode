f9abc1dad554e2f8f8f777795ed17a6d2875423a
David <wendajiang93@163.com>
2022-05-29 16:23:34 +0800

    easy

--- cpp/242.valid-anagram.cpp
+++ cpp/242.valid-anagram.cpp
@@ -1,43 +1,70 @@
 /*
+ * @lc app=leetcode id=242 lang=cpp
+ *
  * [242] Valid Anagram
  *
- * https://leetcode.com/problems/valid-anagram
+ * https://leetcode.com/problems/valid-anagram/description/
  *
- * Easy (46.20%)
- * Total Accepted:
- * Total Submissions:
- * Testcase Example:  '""\n""'
+ * algorithms
+ * Easy (60.28%)
+ * Likes:    4975
+ * Dislikes: 217
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '"anagram"\n"nagaram"'
  *
- * Given two strings s and t, write a function to determine if t is an anagram
- * of s.
+ * Given two strings s and t, return true if t is an anagram of s, and false
+ * otherwise.
  *
- * For example,
- * s = "anagram", t = "nagaram", return true.
- * s = "rat", t = "car", return false.
+ * An Anagram is a word or phrase formed by rearranging the letters of a
+ * different word or phrase, typically using all the original letters exactly
+ * once.
  *
  *
- * Note:
- * You may assume the string contains only lowercase alphabets.
+ * Example 1:
+ * Input: s = "anagram", t = "nagaram"
+ * Output: true
+ * Example 2:
+ * Input: s = "rat", t = "car"
+ * Output: false
  *
- * Follow up:
- * What if the inputs contain unicode characters? How would you adapt your
- * solution to such case?
+ *
+ * Constraints:
+ *
+ *
+ * 1 <= s.length, t.length <= 5 * 10^4
+ * s and t consist of lowercase English letters.
+ *
+ *
+ *
+ * Follow up: What if the inputs contain Unicode characters? How would you
+ * adapt your solution to such a case?
+ *
  */
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
 class Solution {
-public:
+   public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-        map<char,int> count;
-        for(auto e : s){
-            count[e]++;
+        if (s.size() != t.size()) {
+            return false;
         }
-        for(auto e : t){
-            count[e] --;
+
+        unordered_map<char, int> mps;
+        unordered_map<char, int> mpt;
+        for (int i = 0; i < s.size(); i++) {
+            mps[s[i]]++;
+            mpt[t[i]]++;
         }
-        for(auto e : count){
-            if(e.second != 0)
+
+        for (auto p : mps) {
+            if (mpt[p.first] != p.second) {
                 return false;
+            }
         }
         return true;
     }
 };
+// @lc code=end
