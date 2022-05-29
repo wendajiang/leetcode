54e2bfd8d9eca53eb63035922c5aab7378f0e691
David <wendajiang93@163.com>
2022-05-29 15:31:27 +0800

    chore: update 1

--- cpp/1.two-sum.cpp
+++ cpp/1.two-sum.cpp
@@ -1,20 +1,23 @@
 /**
  * @lc app=leetcode id=1 lang=cpp
- * 
+ *
  */
+#include <bits/stdc++.h>
+using namespace std;
 // @lc leetcode=start
 class Solution {
-public:
+   public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
         unordered_map<int, int> mapping;
 
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             mapping[nums[i]] = i;
         }
-        for(int i = 0; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++) {
             int searched = target - nums[i];
-            if(mapping.find(searched) != mapping.end() && i != mapping[searched]){
+            if (mapping.find(searched) != mapping.end() &&
+                i != mapping[searched]) {
                 result.push_back(i);
                 result.push_back(mapping[searched]);
                 break;
