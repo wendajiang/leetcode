9c498007a3e4db3b0ed257ce1f072deb69a1ee70
David <wendajiang93@163.com>
2022-05-29 15:50:17 +0800

    format

--- cpp/10.regular-expression-matching.cpp
+++ cpp/10.regular-expression-matching.cpp
@@ -29,36 +29,44 @@
  * isMatch("aab", "c*a*b") ? true
  *
  */
+
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
-public:
+   public:
     // DP解法
-/*
-This problem has a typical solution using Dynamic Programming. We define the state P[i][j] to be true
-if s[0..i) matches p[0..j) and false otherwise. Then the state equations are:
+    /*
+    This problem has a typical solution using Dynamic Programming. We define the
+    state P[i][j] to be true if s[0..i) matches p[0..j) and false otherwise.
+    Then the state equations are:
 
-P[i][j] = P[i - 1][j - 1], if p[j - 1] != '*' && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
-P[i][j] = P[i][j - 2], if p[j - 1] == '*' and the pattern repeats for 0 times;
-P[i][j] = P[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'), if p[j - 1] == '*' and the pattern repeats for at least 1 times.
+    P[i][j] = P[i - 1][j - 1], if p[j - 1] != '*' && (s[i - 1] == p[j - 1] ||
+    p[j - 1] == '.'); P[i][j] = P[i][j - 2], if p[j - 1] == '*' and the pattern
+    repeats for 0 times; P[i][j] = P[i - 1][j] && (s[i - 1] == p[j - 2] || p[j -
+    2] == '.'), if p[j - 1] == '*' and the pattern repeats for at least 1 times.
 
-Putting these together, we will have the following code.
-*/
-    bool isMatch(string s, string p){
+    Putting these together, we will have the following code.
+    */
+    bool isMatch(string s, string p) {
         int m = s.length(), n = p.length();
-        vector<vector<bool> > dp(m + 1, vector<bool> (n + 1, false));
+        vector<vector<bool> > dp(m + 1, vector<bool>(n + 1, false));
         dp[0][0] = true;
-        for(int i = 0; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(p[j - 1] == '*')
-                    dp[i][j] = dp[i][j - 2] || (i > 0 && dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
-                else 
-                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+        for (int i = 0; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p[j - 1] == '*')
+                    dp[i][j] = dp[i][j - 2] ||
+                               (i > 0 && dp[i - 1][j] &&
+                                (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
+                else
+                    dp[i][j] = i > 0 && dp[i - 1][j - 1] &&
+                               (s[i - 1] == p[j - 1] || p[j - 1] == '.');
             }
         }
         return dp[m][n];
     }
 
-
-    // // 在ide中测试没有问题，递归方法栈深度过深是可能存在的问题，上面使用DP解法
+    // //
+    // 在ide中测试没有问题，递归方法栈深度过深是可能存在的问题，上面使用DP解法
     // bool isMatch(string s, string p) {
     //     if(s.size() == 0 || p.size() == 0)
     //         return false;
@@ -81,7 +89,4 @@ Putting these together, we will have the following code.
     //         return isMatchCore(s, p, si + 1, pi + 1);
     //     return false;
     // }
-
-
-
 };
