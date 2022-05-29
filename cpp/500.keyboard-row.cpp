464cbab40f3d94e06008a4190a5241e969ab5e75
David <wendajiang93@163.com>
2022-05-29 11:41:34 +0800

    easy

--- cpp/500.keyboard-row.cpp
+++ cpp/500.keyboard-row.cpp
@@ -1,55 +1,85 @@
 /*
+ * @lc app=leetcode id=500 lang=cpp
+ *
  * [500] Keyboard Row
  *
- * https://leetcode.com/problems/keyboard-row
+ * https://leetcode.com/problems/keyboard-row/description/
  *
- * Easy (59.98%)
- * Total Accepted:
- * Total Submissions:
+ * algorithms
+ * Easy (67.10%)
+ * Likes:    928
+ * Dislikes: 924
+ * Total Accepted:    153K
+ * Total Submissions: 224.5K
  * Testcase Example:  '["Hello","Alaska","Dad","Peace"]'
  *
- * Given a List of words, return the words that can be typed using letters of
- * alphabet on only one row's of American keyboard like the image
+ * Given an array of strings words, return the words that can be typed using
+ * letters of the alphabet on only one row of American keyboard like the image
  * below.
  *
+ * In the American keyboard:
  *
  *
- *
+ * the first row consists of the characters "qwertyuiop",
+ * the second row consists of the characters "asdfghjkl", and
+ * the third row consists of the characters "zxcvbnm".
  *
  *
  *
  * Example 1:
  *
- * Input: ["Hello", "Alaska", "Dad", "Peace"]
- * Output: ["Alaska", "Dad"]
+ *
+ * Input: words = ["Hello","Alaska","Dad","Peace"]
+ * Output: ["Alaska","Dad"]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: words = ["omk"]
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: words = ["adsdf","sfd"]
+ * Output: ["adsdf","sfd"]
  *
  *
  *
- * Note:
+ * Constraints:
  *
- * You may use one character in the keyboard more than once.
- * You may assume the input string will only contain letters of alphabet.
+ *
+ * 1 <= words.length <= 20
+ * 1 <= words[i].length <= 100
+ * words[i] consists of English letters (both lowercase and uppercase). 
  *
  *
  */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
 class Solution {
-public:
+   public:
     vector<string> findWords(vector<string>& words) {
-        map<char, int> mp{{'q',1},{'Q',1},{'w',1},{'W',1},{'e',1},{'E',1},{'r',1},{'R',1},{'t',1},{'T',1},{'y',1},{'Y',1},
-                            {'u',1},{'U',1},{'i',1},{'I',1},{'o',1},{'O',1},{'p',1},{'P',1},
-                            {'a',2},{'A',2},{'s',2},{'S',2},{'d',2},{'D',2},{'f',2},{'F',2},{'g',2},{'G',2},{'h',2},{'H',2},{'j',2},{'J',2},{'k',2},{'K',2},{'l',2},{'L',2},
-                            {'z',3},{'Z',3},{'x',3},{'X',3},{'c',3},{'C',3},{'v',3},{'V',3},{'b',3},{'B',3},{'n',3},{'N',3},{'m',3},{'M',3}};
+        unordered_map<char, int> mp{
+            {'q', 1}, {'w', 1}, {'e', 1}, {'r', 1}, {'t', 1}, {'y', 1},
+            {'u', 1}, {'i', 1}, {'o', 1}, {'p', 1}, {'a', 2}, {'s', 2},
+            {'d', 2}, {'f', 2}, {'g', 2}, {'h', 2}, {'j', 2}, {'k', 2},
+            {'l', 2}, {'z', 3}, {'x', 3}, {'c', 3}, {'v', 3}, {'b', 3},
+            {'n', 3}, {'m', 3}};
         vector<string> res;
         bool flag = true;
-        for(int i = 0; i < words.size(); i++){
-            int tmp = mp[words[i][0]];
-            for(int j = 0; j < words[i].size(); j++){
-                if(mp[words[i][j]] != tmp)
-                    flag = false;
+        for (int i = 0; i < words.size(); i++) {
+            char c = std::tolower(words[i][0]);
+            int tmp = mp[c];
+            for (int j = 0; j < words[i].size(); j++) {
+                char c1 = std::tolower(words[i][j]);
+                if (mp[c1] != tmp) flag = false;
             }
-            if(!flag){
-
-            }else{
+            if (!flag) {
+            } else {
                 res.push_back(words[i]);
             }
             flag = true;
@@ -57,3 +87,4 @@ public:
         return res;
     }
 };
+// @lc code=end
