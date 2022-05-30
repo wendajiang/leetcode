32088f76db66923fcb723aa5b9c036fab2f9c851
David <wendajiang93@163.com>
2022-05-30 09:12:54 +0800

    newton sqrt

--- cpp/69.sqrtx.cpp
+++ cpp/69.sqrtx.cpp
@@ -1,57 +1,62 @@
 /*
+ * @lc app=leetcode id=69 lang=cpp
+ *
  * [69] Sqrt(x)
  *
- * https://leetcode.com/problems/sqrtx
+ * https://leetcode.com/problems/sqrtx/description/
  *
- * Easy (27.67%)
- * Total Accepted:
- * Total Submissions:
- * Testcase Example:  '0'
+ * algorithms
+ * Easy (36.00%)
+ * Likes:    3902
+ * Dislikes: 3225
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.9M
+ * Testcase Example:  '4'
  *
- * Implement int sqrt(int x).
+ * Given a non-negative integer x, compute and return the square root of x.
  *
- * Compute and return the square root of x.
+ * Since the return type is an integer, the decimal digits are truncated, and
+ * only the integer part of the result is returned.
+ *
+ * Note: You are not allowed to use any built-in exponent function or operator,
+ * such as pow(x, 0.5) or x ** 0.5.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: x = 4
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: x = 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since the decimal part
+ * is truncated, 2 is returned.
+ *
+ *
+ * Constraints:
+ *
+ *
+ * 0 <= x <= 2^31 - 1
+ *
+ *
  */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
 class Solution {
-public:
-    //牛顿法
-    const float EPS = 0.000000001;
-    // int mySqrt(int x) {
-    //     if(x == 0)return x;
-    //     float dividend = x;
-    //     float val = x;
-    //     float last;
-    //     do{
-    //         last = val;
-    //         val = (val + dividend/val) / 2;// 关键代码
-    //     }while (abs(val - last) > EPS);
-    //     int result = val;
-    //     if(result * result > x){
-    //         result --;
-    //     }
-    //     return result;
-    // }
-
+   public:
     int mySqrt(int x) {
         if (x == 0) return x;
         double r = x;
         while (abs(r * r - x) > 0.1) {
             r = r - (r * r - x) / (2 * r);
         }
-
         return r;
     }
-
-    // //http://www.cnblogs.com/ECJTUACM-873284962/p/6536576.html
-    // int sqrt(float x) { 
-        // if(x == 0) return 0; 
-        // float result = x; 
-        // float xhalf = 0.5f*result; 
-        // int i = *(int*)&result; 
-        // i = 0x5f375a86- (i>>1); // what the fuck? 
-        // result = *(float*)&i; 
-        // result = result*(1.5f-xhalf*result*result); // Newton step, repeating increases accuracy 
-        // result = result*(1.5f-xhalf*result*result); 
-        // return 1.0f/result;
-    // }
 };
+// @lc code=end
