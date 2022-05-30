582ed468a76766571e345244b501bf975e5a3217
David <wendajiang93@163.com>
2022-05-30 22:00:39 +0800

    min stack

--- cpp/155.min-stack.cpp
+++ cpp/155.min-stack.cpp
@@ -1,83 +1,102 @@
 /*
+ * @lc app=leetcode id=155 lang=cpp
+ *
  * [155] Min Stack
  *
- * https://leetcode.com/problems/min-stack
+ * https://leetcode.com/problems/min-stack/description/
  *
- * Easy (28.23%)
- * Total Accepted:
- * Total Submissions:
- * Testcase Example:  '["MinStack","push","push","push","getMin","pop","top","getMin"]\n[[],[-2],[0],[-3],[],[],[],[]]'
- *
+ * algorithms
+ * Easy (48.87%)
+ * Likes:    8171
+ * Dislikes: 630
+ * Total Accepted:    992.5K
+ * Total Submissions: 2M
+ * Testcase Example:
+ '["MinStack","push","push","push","getMin","pop","top","getMin"]\n' +
+  '[[],[-2],[0],[-3],[],[],[],[]]'
  *
  * Design a stack that supports push, pop, top, and retrieving the minimum
  * element in constant time.
  *
- *
- * push(x) -- Push element x onto stack.
- *
- *
- * pop() -- Removes the element on top of the stack.
+ * Implement the MinStack class:
  *
  *
- * top() -- Get the top element.
- *
- *
- * getMin() -- Retrieve the minimum element in the stack.
+ * MinStack() initializes the stack object.
+ * void push(int val) pushes the element val onto the stack.
+ * void pop() removes the element on the top of the stack.
+ * int top() gets the top element of the stack.
+ * int getMin() retrieves the minimum element in the stack.
  *
  *
  *
+ * Example 1:
  *
- * Example:
  *
+ * Input
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ *
+ * Output
+ * [null,null,null,null,-3,null,0,-2]
+ *
+ * Explanation
  * MinStack minStack = new MinStack();
  * minStack.push(-2);
  * minStack.push(0);
  * minStack.push(-3);
- * minStack.getMin();   --> Returns -3.
+ * minStack.getMin(); // return -3
  * minStack.pop();
- * minStack.top();      --> Returns 0.
- * minStack.getMin();   --> Returns -2.
+ * minStack.top();    // return 0
+ * minStack.getMin(); // return -2
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * -2^31 <= val <= 2^31 - 1
+ * Methods pop, top and getMin operations will always be called on non-empty
+ * stacks.
+ * At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
  *
  *
  */
+#include <bits/stdc++.h>
+using namespace std;
+// @lc code=start
 class MinStack {
-public:
-    /** initialize your data structure here. */
-    MinStack() {
-    }
+   public:
+    MinStack() {}
 
-    void push(int x) {
-        container.push(x);
-        if(!minStack.empty()){
-            int tmp = minStack.top();
-            minStack.push(std::min(tmp, x));
-        }else{
-            minStack.push(x);
+    void push(int val) {
+        st.push(val);
+        if (st_min.empty()) {
+            st_min.push(val);
+        } else {
+            st_min.push(min(val, st_min.top()));
         }
     }
 
     void pop() {
-        container.pop();
-        minStack.pop();
+        st.pop();
+        st_min.pop();
     }
 
-    int top() {
-        return container.top();
-    }
+    int top() { return st.top(); }
 
-    int getMin() {
-        return minStack.top();
-    }
-private:
-    stack<int> container;
-    stack<int> minStack;
+    int getMin() { return st_min.top(); }
+
+   private:
+    stack<int> st;
+    stack<int> st_min;
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
  */
+// @lc code=end
