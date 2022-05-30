57db689f03f92b113ec41283f0741b1213400c53
David <wendajiang93@163.com>
2022-05-30 09:17:59 +0800

    pointer

--- cpp/237.delete-node-in-a-linked-list.cpp
+++ cpp/237.delete-node-in-a-linked-list.cpp
@@ -1,40 +1,73 @@
 /*
+ * @lc app=leetcode id=237 lang=cpp
+ *
  * [237] Delete Node in a Linked List
  *
- * https://leetcode.com/problems/delete-node-in-a-linked-list
+ * https://leetcode.com/problems/delete-node-in-a-linked-list/description/
  *
- * Easy (46.23%)
- * Total Accepted:    
- * Total Submissions: 
- * Testcase Example:  '[0,1]\nnode at index 0 (node.val = 0)'
+ * algorithms
+ * Easy (70.08%)
+ * Likes:    4519
+ * Dislikes: 11771
+ * Total Accepted:    809.2K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[4,5,1,9]\n5'
  *
- * 
- * Write a function to delete a node (except the tail) in a singly linked list,
- * given only access to that node.
- * 
- * 
- * 
- * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third
- * node with value 3, the linked list should become 1 -> 2 -> 4 after calling
- * your function.
- * 
+ * Write a function to delete a node in a singly-linked list. You will not be
+ * given access to the head of the list, instead you will be given access to
+ * the node to be deleted directly.
+ *
+ * It is guaranteed that the node to be deleted is not a tail node in the
+ * list.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [4,5,1,9], node = 5
+ * Output: [4,1,9]
+ * Explanation: You are given the second node with value 5, the linked list
+ * should become 4 -> 1 -> 9 after calling your function.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [4,5,1,9], node = 1
+ * Output: [4,5,9]
+ * Explanation: You are given the third node with value 1, the linked list
+ * should become 4 -> 5 -> 9 after calling your function.
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of the nodes in the given list is in the range [2, 1000].
+ * -1000 <= Node.val <= 1000
+ * The value of each node in the list is unique.
+ * The node to be deleted is in the list and is not a tail node
+ *
+ *
  */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// @lc code=start
+
 class Solution {
-public:
+   public:
     void deleteNode(ListNode* node) {
-        if(node->next == NULL){
-            node = NULL;
-        }else{
-            node->val = node->next->val;
-            node->next = node->next->next;
-        }
+        ListNode* next = node->next;
+        node->val = next->val;
+        node->next = next->next;
     }
 };
+// @lc code=end
