d32efaed0135af03cb8e316cf4d8bb256c4c6e73
David <wendajiang93@163.com>
2022-09-02 11:29:20 +0800

    chore: update 122

--- cpp/122.best-time-to-buy-and-sell-stock-ii.cpp
+++ cpp/122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,35 +1,66 @@
-/*
- * [122] Best Time to Buy and Sell Stock II
- *
- * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
- *
- * Easy (46.69%)
- * Total Accepted:
- * Total Submissions:
- * Testcase Example:  '[]'
- *
- * Say you have an array for which the ith element is the price of a given
- * stock on day i.
- *
- * Design an algorithm to find the maximum profit. You may complete as many
- * transactions as you like (ie, buy one and sell one share of the stock
- * multiple times). However, you may not engage in multiple transactions at the
- * same time (ie, you must sell the stock before you buy again).
- */
+//  Category: algorithms
+//  Level: Medium
+//  Percent: 62.890446%
+
+//  You are given an integer array prices where prices[i] is the price of a
+//  given stock on the ith day.
+//
+//  On each day, you may decide to buy and/or sell the stock. You can only hold
+//  at most one share of the stock at any time. However, you can buy it then
+//  immediately sell it on the same day.
+//
+//  Find and return the maximum profit you can achieve.
+//
+//   
+//  Example 1:
+//
+//  Input: prices = [7,1,5,3,6,4]
+//  Output: 7
+//  Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit
+//  = 5-1 = 4. Then buy on day 4 (price = 3) and sell on day 5 (price = 6),
+//  profit = 6-3 = 3. Total profit is 4 + 3 = 7.
+//
+//
+//  Example 2:
+//
+//  Input: prices = [1,2,3,4,5]
+//  Output: 4
+//  Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit
+//  = 5-1 = 4. Total profit is 4.
+//
+//
+//  Example 3:
+//
+//  Input: prices = [7,6,4,3,1]
+//  Output: 0
+//  Explanation: There is no way to make a positive profit, so we never buy the
+//  stock to achieve the maximum profit of 0.
+//
+//
+//   
+//  Constraints:
+//
+//
+//  	1 <= prices.length <= 3 * 10⁴
+//  	0 <= prices[i] <= 10⁴
+//
+#include <bits/stdc++.h>
+using namespace std;
+//  @lc code=start
 class Solution {
-public:
+   public:
     // 不用求差值数组，可以直接求
     int maxProfit(vector<int>& prices) {
-        if(!prices.size()) return 0;
+        if (!prices.size()) return 0;
         vector<int> diff;
-        for(int i = 1; i < prices.size(); i++){
-            diff.push_back(prices[i] - prices[i-1]);
+        for (int i = 1; i < prices.size(); i++) {
+            diff.push_back(prices[i] - prices[i - 1]);
         }
         int profit = 0;
-        for(auto x : diff){
-            if(x > 0)
-                profit += x;
+        for (auto x : diff) {
+            if (x > 0) profit += x;
         }
         return profit;
     }
 };
+//  @lc code=end
